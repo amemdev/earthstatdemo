9ecbddeeb2a7257883b65c3ec65ea44efdf36611
amemndev <amemndev>
2024-01-29 17:46:52 +1100

    feat: add models: DataSet, GeoDatum

--- earthstatdemo/dataset_api/models.py
+++ earthstatdemo/dataset_api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Dataset(models.Model):
+    title = models.CharField(max_length=500)
+
+class GeoDatum(models.Model):
+    dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE)
+    label = models.CharField(max_length=500, blank=True)
+    lng = models.DecimalField(max_digits=9, decimal_places=6)
+    lat = models.DecimalField(max_digits=8, decimal_places=6)
+    height = models.DecimalField(max_digits=10, decimal_places=6)
+    datum = models.DecimalField(max_digits=15, decimal_places=6)
