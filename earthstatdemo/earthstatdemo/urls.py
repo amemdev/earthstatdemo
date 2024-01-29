5552b11dc3ee88f0a6455052fe2fb16893638d24
amemndev <amemndev>
2024-01-29 19:13:07 +1100

    feat: add placeholder for site frontend

--- earthstatdemo/earthstatdemo/urls.py
+++ earthstatdemo/earthstatdemo/urls.py
@@ -1,8 +1,11 @@
 from dataset_api import urls as dataset_api_urls
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('api/', include(dataset_api_urls)),
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(
+        template_name='site/app.html'), name='app-page'),
 ]
