42f33c54deef78f497b64e7d57284956725376f2
amemndev <amemndev>
2024-01-29 21:35:52 +1100

    feat: re-add placeholder react app

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,7 @@
+/**
+ * Set up Cesium
+ */
+
 window.CESIUM_BASE_URL = "/static/cesiumStatic";
 
 import {
@@ -15,3 +19,19 @@ Ion.defaultAccessToken = process.env.CESIUM_ACCESS_TOKEN;
 const viewer = new Viewer("cesiumContainer", {
   terrain: Terrain.fromWorldTerrain(),
 });
+
+/**
+ * Set up React App
+ */
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./app.js";
+
+const root = createRoot(document.getElementById("reactApp"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
